5a5e13e62533a4a9c4faf6c966abed83b5487f9c
ValenPuente <valenipuente@hotmail.com>
2025-05-25 18:25:13 -0300

    Agregué métodos a la interfaz IÁrbol

--- src/interfaces/IArbol.java
+++ src/interfaces/IArbol.java
@@ -1,5 +1,15 @@
 package interfaces;
 
 public interface IArbol {
+	
+	public void insertar(IPersona persona);
+
+	public INodo insertarRec(INodo nodo, IPersona persona);
+	
+	public void preOrder(INodo nodo);
+	
+	public void inOrder(INodo nodo);
+	
+	public void postOrder(INodo nodo);
 
 }
